4b3a1aac9cbb976940cf40cb29b553d93008c968
Jamie Kear <dkear@oswego.edu>
2017-10-24 09:11:59 -0400

    1

---
+++ src/User.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class User implements Runnable {
+
+    final Random random = new Random();
+    final int requiredSeats = 0;
+    final String[] firstNames = {"John", "James", "Bill", "David", "Doug", "Chris", "Dan", "Tom", "Clayton", "Connor", "Sarah", "Nikki", "Madison", "Kim", "Michelle", "Olivia", "Elizabeth", "Alicia", "Viana", "Christine"};
+    final String[] lastNames = {"Doe", "Jones", "West", "Long", "Johnson", "White", "Walker", "Brown", "Clark", "Wilson"};
+    String firstName;
+    String lastName;
+    public void run(){
+
+    }
+}
